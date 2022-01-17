a68e550325cd7823524e7a5f8559b756abe2c49b
Crowdbotics <team@crowdbotics.com>
2022-01-17 19:05:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sparkling_breeze_32912",
+  url: "https://sparkling_breeze_32912.botics.co",
+  api: "https://sparkling_breeze_32912.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
